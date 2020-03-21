24e9cf39351014b14fe8ed23f8402de3e17b0dbc
Eka Putra <eka@insight.co>
2020-03-21 13:50:49 +1100

    Added firestore event handlers

---
+++ example/functions/index.js
@@ -0,0 +1,9 @@
+const admin = require('firebase-admin')
+const firepubs = require('firepubs')
+
+admin.initializeApp()
+
+exports.firepubsDemo = firepubs.FirestoreOnWrite(
+  '/env/eka/firepubs/{docId}',
+  'firepubs'
+)
